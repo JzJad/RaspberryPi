6c52ab655138bd4370c4a1fd68e9d35e3061d9fe
Jimmy <jimmy.jad@gmail.com>
2015-12-04 11:04:02 -0600

    Updated

--- Scripts/Transmission.py
+++ Scripts/Transmission.py
@@ -18,7 +18,7 @@ rpcPass = getpass.getpass("Password: ")
 '''
 #tc = transmissionrpc.Client(address=rpcUrl, port=rpcPort, user=rpcUser, password=rpcPass)
 
-tc = transmissionrpc.Client('192.168.0.45', port=9091, user='has', password='Simplicity316')
+tc = transmissionrpc.Client('192.168.0.45', port=9091, user='has', password='asdf123')
 torrent = tc.get_torrent(2)
 status = torrent.status
 
