eb6177da6b7c3bc777f9fa1d8e87a460664046d8
Jimmy <jimmy.jad@gmail.com>
2015-12-04 11:21:16 -0600

    Added GPIO stuff

--- Scripts/transmissionStatus.py
+++ Scripts/transmissionStatus.py
@@ -2,32 +2,40 @@
 import transmissionrpc
 import getpass
 #End imports
-'''
+
+
+
+#LED Mapping
+red = 22
+green = 23
+blue = 24
 #=== GPIO Setup ===#
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
+#Color Sets
+setRed = GPIO.output(red, 1)
+setBlue = GPIO.output(green, 1)
+setGreen = GPIO.output(blue, 1)
+setYellow = GPIO.cleanup()
 #======End GPIO Setup======#
-'''
-
-#rpcUrl = raw_input("Transmission URL: ")
-rpcUrl = "192.168.0.45"
+rpcUrl = raw_input("Transmission URL: ")
 rpcPort = raw_input("Port: ")
 rpcUser = raw_input("Username: ")
 rpcPass = getpass.getpass("Password: ")
 #=============================#
-'''
-#tc = transmissionrpc.Client(address=rpcUrl, port=rpcPort, user=rpcUser, password=rpcPass)
-
-tc = transmissionrpc.Client('192.168.0.45', port=9091, user='has', password='asdf123')
-torrent = tc.get_torrent(2)
+tc = transmissionrpc.Client(address=rpcUrl, port=rpcPort, user=rpcUser, password=rpcPass)
+torrent = tc.get_torrent(1)
 status = torrent.status
-
 #============================#
 if status == "stopped":
-   print torrent.name,"  :Torrent stopped!"
+   setRed
 elif status == "download pending":
-   print "Download Queued"
+   setBlue
 elif status == "paused":
-   print "Download Paused"
+   setYellow
 else:
-   print "Torrent doing something else?"
+   setGreen
+   GPIO.cleanup()
